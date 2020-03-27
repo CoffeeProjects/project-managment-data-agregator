2415db209406443d7bfe0a207ebdf9b902cbeb96
Julien Herubel <jherubel@ippon.fr>
2020-03-27 14:34:38 +0100

    Initialisation du projet

---
+++ src/main/java/org/coffeeprojects/pmda/PmdaApplication.java
@@ -0,0 +1,13 @@
+package org.coffeeprojects.pmda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/coffeeprojects/pmda/PmdaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coffeeprojects.pmda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
