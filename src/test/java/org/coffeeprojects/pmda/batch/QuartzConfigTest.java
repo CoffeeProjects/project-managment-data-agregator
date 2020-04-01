9c2f40eb4657c78b27ee925910eabd4de65bd0d4
Julien Herubel <jherubel@ippon.fr>
2020-04-01 17:30:41 +0200

    Intégration de Spring Quartz

---
+++ src/test/java/org/coffeeprojects/pmda/batch/QuartzConfigTest.java
@@ -0,0 +1,31 @@
+package org.coffeeprojects.pmda.batch;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.quartz.CronExpression;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class QuartzConfigTest {
+
+    @Test
+    public void cron_expression_is_valid() throws ParseException {
+        String exp = "0 0/1 * 1/1 * ? *";
+        boolean isValidExpression = CronExpression.isValidExpression(exp);
+
+        Calendar date = new GregorianCalendar(2020, 1, 1, 23, 00,00);
+        Calendar expectedDate = new GregorianCalendar(2020, 1, 1, 23, 01,00);
+
+        CronExpression cronExpression = new CronExpression(exp);
+        cronExpression.getNextValidTimeAfter(date.getTime());
+
+        assertThat(cronExpression.getNextValidTimeAfter(date.getTime()).getTime()).isEqualTo(expectedDate.getTime().getTime());
+        assertThat(isValidExpression).isTrue();
+    }
+}
