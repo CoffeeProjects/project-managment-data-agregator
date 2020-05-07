5b65197ee130be860008e63acc32ac333a91d1df
Julien Herubel <julien@herubel.fr>
2020-05-07 20:10:06 +0200

    StackOverflowError (recursive IssueEntity in Components) (#16)
    * Gestion de StackOverFlowError à cause de la recursivité IssueEntity dans les Components
    
    * Suppression des imports non utilisés

--- src/main/java/org/coffeeprojects/pmda/feature/component/ComponentEntity.java
+++ src/main/java/org/coffeeprojects/pmda/feature/component/ComponentEntity.java
@@ -1,23 +1,17 @@
 package org.coffeeprojects.pmda.feature.component;
 
 import org.coffeeprojects.pmda.entity.BaseEntity;
-import org.coffeeprojects.pmda.feature.issue.IssueEntity;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
 @Table(name = "Component")
 public class ComponentEntity extends BaseEntity implements Serializable {
 
     private String name;
-
-    @ManyToMany(mappedBy="components")
-    private Set<IssueEntity> issues;
 
     public String getName() {
         return name;
@@ -28,35 +22,24 @@ public class ComponentEntity extends BaseEntity implements Serializable {
         return this;
     }
 
-    public Set<IssueEntity> getIssues() {
-        return issues;
-    }
-
-    public ComponentEntity setIssues(Set<IssueEntity> issues) {
-        this.issues = issues;
-        return this;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         ComponentEntity that = (ComponentEntity) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(issues, that.issues);
+        return Objects.equals(name, that.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), name, issues);
+        return Objects.hash(super.hashCode(), name);
     }
 
     @Override
     public String toString() {
         return "ComponentEntity{" +
                 "name='" + name + '\'' +
-                ", issues=" + issues +
                 '}';
     }
 }
